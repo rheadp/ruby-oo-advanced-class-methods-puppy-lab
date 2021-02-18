2279de0d7b0953a2c22687ef7dcae84402a26816
rheadp <rheadpea@msu.edu>
2021-02-17 20:39:10 -0500

    commit tree 007ccd7552a1d83ace8d0c982a74c6debf10cd5e

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
-# Add your code here
+class Dog
+  @@all = []
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    self.save
+
+  end
+
+  def self.all
+    @@all #returns all dog instances
+  end
+  
+  def self.clear_all
+    @@all = []
+  end
+
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name #class method that puts out the name of each dog to the terminal 
+    end
+  end
+  
+  def save
+    @@all << self
+  end
+
+
+end
